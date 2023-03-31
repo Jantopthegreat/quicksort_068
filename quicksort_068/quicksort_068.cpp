ddfb009914a9a9718362f4d06732fe36fd6b115b
Jantopthegreat <fauzangriya@gmail.com>
2023-03-31 15:20:45 +0700

    memindahkah pivot ke tempat yang benar

--- quicksort_068/quicksort_068.cpp
+++ quicksort_068/quicksort_068.cpp
@@ -73,10 +73,16 @@ void q_short(int low, int high)
             // Swap the element at index i with the element at index j
             swap(i, j);
             mov_count++;
-        }
+        } 
     }
-    
-
+    // J now containt the index of the last element in the sorted list
+    if (low < j);                                               // Langkah Algoritma No.11
+    {
+        // Move the pivot to its correct position in the list
+        swap(low, j);
+        mov_count++;
+    }
+   
    
 
 
